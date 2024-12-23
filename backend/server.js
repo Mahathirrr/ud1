88a491835ece676cef1071c21dc2b1359602a4ef
emha arch <mhmmdmhthr@gmail.com>
2024-12-23 15:26:45 +0700

    init

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,37 @@
-import express from 'express';
-import { readdirSync } from 'fs';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-
-const logger = require('morgan');
+import express from "express";
+import { readdirSync } from "fs";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import morgan from "morgan";
 
 // Create express app
 const app = express();
 dotenv.config();
 
 mongoose
-  .connect(process.env.DATABASE)
-  .then(() => console.log('DB Connected'))
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
   .catch((e) => console.error(e));
 
 // Apply Middlewares
-app.use(logger('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      'http://localhost:3000',
-      'https://learnlit.vercel.app',
-      'https://learnlit.tech',
-    ],
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
 
-app.get('/', (req, res) => {
-  res.send('API available at /api');
+app.get("/", (req, res) => {
+  res.send("API available at /api");
 });
 
-readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 const PORT = process.env.PORT || 8000;
 
